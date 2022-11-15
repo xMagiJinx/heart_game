c59aca56397dce68f04b2719303df3f18228faf9
xMagiJinx <m255448@usna.edu>
2022-11-15 09:45:35 -0500

    game for class

---
+++ heart_game.py
@@ -0,0 +1,12 @@
+import sys
+
+import pygame
+
+import time
+import random
+import math
+
+class HeartGame:
+    """Create a main heart game"""
+    def __init__(self):
+        """Initialize the game"""
