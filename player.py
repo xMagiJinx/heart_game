bbb1e108889ee6b51a5081de35a5abe41095a8a4
xMagiJinx <m255448@usna.edu>
2022-11-16 08:45:38 -0500

    settings

--- player.py
+++ player.py
@@ -8,4 +8,23 @@ class Player:
         self.screen_rect = heart_game.get_rect()
 
         self.image = pygame.image.load('images/Icon_Heart.png')
-        self.rect =
+        self.rect = self.image.get_rect()
+
+        self.center_heart()
+
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the heart and movement"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.heart_speed
+
+    def center_heart(self):
+        """Keep the heart at the bottomcenter of the screen"""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
+    def blitme(self):
+        """Draw the heart"""
+        self.screen.blit(self.image, self.rect)
--- settings.py
+++ settings.py
@@ -4,3 +4,11 @@ class Settings:
         self.screen_width = 1000
         self.screen_height = 800
         self.bg_color = (0,0,0)
+
+        self.heart_speed = 3
+        self.heart_limit = 3
+
+        # Obstacle settings for red flags
+        self.flag_appear = .02
+        self.flag_speed = .5
+
